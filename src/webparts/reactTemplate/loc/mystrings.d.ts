c5c9100d292c2835e757888487b5ee4d9ad152a6
Jason Clark <jclark@maximusuk.co.uk>
2022-04-29 12:11:07 +0100

    first commit

---
+++ src/webparts/reactTemplate/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactTemplateWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactTemplateWebPartStrings' {
+  const strings: IReactTemplateWebPartStrings;
+  export = strings;
+}
